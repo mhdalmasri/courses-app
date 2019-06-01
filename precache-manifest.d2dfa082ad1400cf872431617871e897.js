eca1dd5a4c4c01cd23b578c8f18b03998eb1d897
mhdalmasri <m.almasri@outlook.de>
2019-06-01 20:49:22 +0200

    Updates

---
+++ precache-manifest.d2dfa082ad1400cf872431617871e897.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1168ef6c28a26db55e4",
+    "url": "/courses-app/static/js/runtime~main.1a24c081.js"
+  },
+  {
+    "revision": "1c0b18ea4c6257a4f880",
+    "url": "/courses-app/static/js/main.a82ed085.chunk.js"
+  },
+  {
+    "revision": "38e9280959dd5f0d5c78",
+    "url": "/courses-app/static/js/2.88ea1fd6.chunk.js"
+  },
+  {
+    "revision": "1c0b18ea4c6257a4f880",
+    "url": "/courses-app/static/css/main.2586b740.chunk.css"
+  },
+  {
+    "revision": "63220ec4d2bceba92638b7aeee18204d",
+    "url": "/courses-app/index.html"
+  }
+];
